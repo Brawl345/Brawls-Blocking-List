960e43a5a5baa4928d043bd8f2a590d31a5282c4
Andreas Bielawski <brawl98@outlook.com>
2015-11-26 18:48:28 +0100

    Initialer Commit

---
+++ genlist/project.clj
@@ -0,0 +1,10 @@
+(defproject genlist "0.1.0-SNAPSHOT"
+  :description "tool to generate a valid adblockplus easylist"
+  :url "http://github.com/r4vi/block-the-eu-cookie-shit-list"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.cli "0.2.1"]
+                 [clj-time "0.4.3"]
+                 [clj-message-digest "1.0.0"]]
+  :main genlist.core)
